6349bd36d4c4f97d3ad820f38e88738349683120
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-05-04 23:27:26 -0500

    Create BurbleSort.java

---
+++ BurbleSort.java
@@ -0,0 +1,16 @@
+package algoritmos_ordenamiento;
+
+public class BurbleSort{
+
+  static int[] Burble_method(int vector1[]){
+    pass
+    return vector1;
+  }
+
+
+  
+  public static void main(String[] args){
+    int vector1[] = {11, 5, 6, 3, 8, 2, 10, 7, 8, 32};
+    vector1_sort = Burble_method(vector1);
+  }
+}
