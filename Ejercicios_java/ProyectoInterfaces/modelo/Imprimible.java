bb1b52d53ea5f86f49021edec56f71e098150de8
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-08-21 00:06:51 -0500

    new features created it

--- Ejercicios_java/ProyectoInterfaces/modelo/Imprimible.java
+++ Ejercicios_java/ProyectoInterfaces/modelo/Imprimible.java
@@ -3,8 +3,13 @@ package ProyectoInterfaces.modelo;
 public interface Imprimible {
     // Todos los metodos de la interfaz se deben imprimir pero se pueden
     // establecer metodos que no sean obligatorios imprimir
+    final static String TEXTO_DEFECTO = "Imprimiento un valor por defecto"; //Scope Global
+
     default String imprimir(){
-        return "Imprimiendo un valor por defecto";
+        return TEXTO_DEFECTO;
     }
 
+    static void imprimible(Imprimible imprimible){
+        System.out.println(imprimible.imprimir());
+    }
 }
--- Ejercicios_java/ProyectoInterfaces/modelo/mainImpresora.java
+++ Ejercicios_java/ProyectoInterfaces/modelo/mainImpresora.java
@@ -4,33 +4,45 @@ import static  ProyectoInterfaces.modelo.Genero.*;
 
 public class mainImprenta {
     public static void main(String[] args){
-        Curriculo cv = new Curriculo("Pablo Cesar Garzon", "Ingeniero de Telecomunicaciones",
+        Curriculo cv = new Curriculo(new Persona("Pablo Cesar","Garzon"), "Ingeniero de Telecomunicaciones",
                 "Resumen laboral ...");
-        cv.addExperiencia("Java");
-        cv.addExperiencia("Oracle DBA");
-        cv.addExperiencia("Spring Framework");
-        cv.addExperiencia("Desarrollador Fullstack");
-        cv.addExperiencia("Angular");
+        cv.addExperiencia("Java")
+        .addExperiencia("Oracle DBA")
+        .addExperiencia("Spring Framework")
+        .addExperiencia("Desarrollador Fullstack")
+        .addExperiencia("Angular");
 
-        Libro libro = new Libro("Erich Gamma", "Patrones de disenos: Elem. Reusables POO",
+        Libro libro = new Libro(new Persona("Erick", "Gamma"), "Patrones de disenos: Elem. Reusables POO",
                 PROGRAMACION);
         libro.addPagina(new Pagina("Patron Singleton"))
                 .addPagina(new Pagina("Patron Observador"))
                 .addPagina(new Pagina("Composite"))
                 .addPagina(new Pagina("Patron Factory"))
                 .addPagina(new Pagina("Patron Fascade"));
-        
-        Informe informe = new Informe("Jorge crauge", "Martin Fownler", "Estudio sobre microservicios");
+
+        Informe informe = new Informe(new Persona("Jorge", "Crauge"),
+                new Persona("Martin", "Fouler"),
+                "Estudio sobre microservicios");
         // Imprimir en pantalla la informacion
-        imprimir(cv);
-        imprimir(informe);
+        Imprimible.imprimible(cv);
+        Imprimible.imprimible(cv); // Emplea metodo estatico desde interfaz
+        imprimir(informe);  // Implementa metodo estatico desde la propia clase
+        System.out.println(Imprimible.TEXTO_DEFECTO);
+
+        // Implementacion de clase anonima
+        Imprimible objImp = new Imprimible() {
+            @Override
+            public String imprimir() {
+                return "Hola que tal, imprimiendo un objeto generico de una clase anonima";
+            }
+        };
+        imprimir(new Imprimible() {
+            // Imprime texto por defecto interno
+        });
     }
     // Metodo estatico de impresion de hoja
-    public static void imprimir(Hoja imprimible){
+    public static void imprimir(Imprimible imprimible){
         System.out.println(imprimible.imprimir());  // Invoca el metodo de la clase abstracta
     }
-
-
-
-
 }
+
