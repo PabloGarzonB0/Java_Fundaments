5d80479b2c35b7878f692b1e8257681e80b45afe
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-08-22 23:23:31 -0500

    adding Listar method in ClienteListRepositorio.java

--- Ejercicios_java/ProyectoInterfaces/Inferface_Repository/repositorio/ClienteListRepositorio.java
+++ Ejercicios_java/ProyectoInterfaces/Inferface_Repository/repositorio/ClienteListRepositorio.java
@@ -53,4 +53,32 @@ public class ClienteListRepositorio implements CrudRepositorio, OrdenableReposit
     public List<Cliente> listar(int desde, int hasta) {
         return List.of();
     }
+
+    @Override
+    public List<Cliente> Listar(String campo, Direccion dir) {
+       dataSource.sort(new Comparator<Cliente>() {
+           @Override
+           public int compare(Cliente a, Cliente b) {
+               int resultado = 0;
+               if(dir == Direccion.ASC){
+                    switch (campo){
+                        case "id" -> resultado = a.getId().compareTo(b.getId());
+                        case "nombre" -> resultado = a.getNombre().compareTo(b.getNombre());
+                        case "apellido" -> resultado = a.getApellido().compareTo(b.getApellido());
+                    }
+               }else if(dir == Direccion.DESC){
+                   switch (campo){
+                       case "id" -> resultado = b.getId().compareTo(a.getId());
+                       case "nombre" -> resultado = b.getNombre().compareTo(a.getNombre());
+                       case "apellido" -> resultado = b.getApellido().compareTo(a.getApellido());
+                   }
+               }
+               return 0;
+           }
+       });
+        return dataSource;
+    }
+
+
+
 }
