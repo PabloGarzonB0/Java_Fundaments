9227c6fd2104324ad3785fa76ab7e9839272b846
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-09-04 00:54:18 -0500

    new features

--- Ejercicios_java/Exceptions/EjemploExcepciones.java
+++ Ejercicios_java/Exceptions/EjemploExcepciones.java
@@ -3,22 +3,26 @@ package Java_Oracle.Exceptions.PooException;
 import java.util.Scanner;
 
 public class EjemploExcepciones {
-    public static Scanner lectura = new Scanner(System.in);
+    static Scanner lectura = new Scanner(System.in);
 
     public static void main(String[] args){
 
         Calculadora cal = new Calculadora();
-        System.out.print("Ingrese el  numero entero divisor: ");
-        String valor = lectura.nextLine();
+        System.out.print("Ingrese un numero entero numerador: ");
+        String numerador = lectura.nextLine();
+
+        System.out.print("Ingrese un  numero entero denominador: ");
+        String denominador = lectura.nextLine();
         double division; int divisor;
         try{
-            divisor = Integer.parseInt(valor);
-            division = cal.dividir(10, divisor);
-            System.out.println(division);
+            double division2 = cal.dividir(numerador,denominador);
+            System.out.println("Division2 = " + division2);
         } catch (NumberFormatException nfe) {
             System.out.println("Se detecto una excepcion, Ingrese un valor numerico " + nfe.getMessage());
             main(args); // Termina la ejecucion de la operacion
-
+        }catch(FormatoNumeroException e){
+            System.out.println("Se detecto una excepcion: Ingrese un numero valido: " + e.getMessage());
+            e.printStackTrace(System.out);
         }catch (DivisionPorZeroException dz){
             System.out.println("Captura de la excepcion en tiempo de ejecucion: " + dz.getMessage());
             main(args); // Termina la ejecucion de la operacion
