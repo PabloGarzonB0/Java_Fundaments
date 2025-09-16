26d86fbf2c5505615008ec045b8bf0fd47e7a866
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-09-16 00:08:34 -0500

    Update mediaEdadPais.java

--- Ejercicios_java/mediaEdadPais.java
+++ Ejercicios_java/mediaEdadPais.java
@@ -2,6 +2,14 @@ package variables;
 
 public class EncuestaPorEdades {
     // Metodos
+    public static double calcMediaEdad(int[] edades){
+        int sumaEdades = 0;
+        for(int edad : edades){
+            sumaEdades += edad;
+        }
+        return (double) sumaEdades / edades.length;
+    }
+
     public static int countElectPais(String[] paises, String paisE){
         int contador = 0;
         for(String pais : paises){
@@ -11,7 +19,6 @@ public class EncuestaPorEdades {
         }
         return contador;
     }
-
     public static double mediaEdadPaisEdad(int[] edades, String[] paises, String paisE){
         int sumaEdades = 0;
         int contador = 0;
@@ -27,8 +34,23 @@ public class EncuestaPorEdades {
         return (double) sumaEdades / contador;
     }
 
-    
+    public static void main(String[] args){
+        // Datos de los encuestados
+        int[] edades = {25, 300, 28, 35, 22, 40};
+        System.out.print("Arreglo edades\n");
 
+        for(int edad: edades){ System.out.println("\t" + edad); }
 
-
+        String[] paises = {"Argentina", "Brasil","Chile", "Argentina", "Peru", "Argentina"};
+        // 1. Promedio de edad de los encuestados
+        double promedioEdades = calcMediaEdad(edades);
+        System.out.println("\n1. Promedio de edad de los encuestados: " + promedioEdades);
+        // 2. Cuantos encuestados eligieron un pais dado
+        String paisBuscado = "Argentina";
+        int cantidadElegidos = countElectPais(paises, paisBuscado);
+        System.out.println("2. Encuestados que eligieron " + paisBuscado + ": " + cantidadElegidos);
+        //3. Promedio de edad de los encuentados conmenos de 30 anios
+        double mediaEPE = mediaEdadPaisEdad(edades, paises, paisBuscado);
+        System.out.println("3. Promedio de edad de encuestados de hasta 30 anios que eligieron " + paisBuscado + ": " + mediaEPE);
+    }
 }
